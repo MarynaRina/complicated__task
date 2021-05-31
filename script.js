649352d3b48b33b39d5dffcde7a5a5ee91660edb
Maryna Maksymchuk <risaranma@gmail.com>
2021-05-31 20:10:43 +0300

    задание 3

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 "use strict";
 
-const num = 266219;
+let lang = prompt('Выберите язык');
 
-const n = num.toString().split('');
-console.log(n);
+if (lang === 'ru') {
+    console.log(`Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье`);
+} else if (lang === 'en') {
+    console.log(`Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday`);
+} else {
+    console.log('Язык не найден');
+}
 
-const mult = n.reduce(function(total, amount){
-    return total * amount;
-});
-console.log(mult);
+switch (lang) {
+  case 'ru':
+    alert( 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье' );
+    break;
+  case 'en':
+    alert( 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday' );
+    break;
+}
 
-const cube = mult ** 3;
-console.log('Полученное число в кубе:' + ' ' + cube);
+let ru = [
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+];
+let en = [
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+]
 
-console.log(String(cube).substr(0, 2));
+let namePerson = prompt('Введите имя');
+console.log((namePerson === 'Максим') ? 'Преподаватель' : (namePerson ==='Артём') ? 'Директор' : 'Студент');
+            
