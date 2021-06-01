91645e6d7922c7387c0aaf46609fdad38161388a
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-01 17:53:34 +0300

    Задание 4 усложненное

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 "use strict";
 
-let lang = prompt('Выберите язык');
+const func = function(argument) {
+  if (typeof argument !== 'string') {
+    return('Это не строка!!!');
+  }
+  argument = argument.trim(); 
+  if (argument.length > 30) {
+    return argument.slice(0, 30) + '...';
+  }
+  else {
+    return argument;
+  }
+}; 
 
-//A
-if (lang === 'ru') {
-    console.log(`Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье`);
-} else if (lang === 'en') {
-    console.log(`Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday`);
-} else {
-    console.log('Язык не найден');
-}
-
-// B
-switch (lang) {
-  case 'ru':
-    alert( 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье' );
-    break;
-  case 'en':
-    alert( 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday' );
-    break;
-}
-
-// C
-const allLang = [
-                ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'], 
-                ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-];
-console.log((lang === 'ru') ? allLang[0] : (lang === 'en') ? allLang[1] : 'Выберите язык');
-
-
-let namePerson = prompt('Введите имя');
-console.log((namePerson === 'Максим') ? 'Преподаватель' : (namePerson ==='Артём') ? 'Директор' : 'Студент');
-            
+console.log(func(3453535));
+console.log(func('    Обычная строка    '));
+console.log(func('    Очень очень очень очень очень очень очень очень длинная строка'));
