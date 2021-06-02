42ac4ec1adb497f5166cdea2e166d90286fcdc57
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-02 22:46:14 +0300

    Усложненное задание 5

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 "use strict";
 
-const func = function(argument) {
-  if (typeof argument !== 'string') {
-    return('Это не строка!!!');
-  }
-  argument = argument.trim(); 
-  if (argument.length > 30) {
-    return argument.slice(0, 30) + '...';
-  }
-  else {
-    return argument;
-  }
-}; 
+let arr = ['23423', '97845', '32346', '54357', '46896', '64783', '134511'];
 
-console.log(func(3453535));
-console.log(func('    Обычная строка    '));
-console.log(func('    Очень очень очень очень очень очень очень очень длинная строка'));
+//  1.
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
+    }
+}
+
+//  2. 
+const num = function(elem) {
+    let arr = [];
+    if (elem !== 1) {
+        arr.push(1);
+        for (let n = 2; n * n <= elem; n++) {
+            if (elem % n === 0) {
+                arr.push(n);
+            }
+        }
+    }
+    arr.push(elem);
+    return arr;
+};
+
+for (let i = 2; i <= 100; i++) {
+    const n = num(i);
+    if (n.length <= 2) {
+        console.log(`${i}: Делители этого числа: ${num(i).join(', ')}`);
+    }
+}
+
