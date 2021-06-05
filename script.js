d4759bcbbc2daaaa4e6ece9068deb9a87779277d
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-05 11:42:34 +0300

    Сделала усложненное задание

--- script.js
+++ script.js
@@ -1,59 +1,28 @@
 'use strict';
 
-// Рандомное число
-const randomNum = function(maxNum) {
-    return Math.floor(Math.random() * Math.floor(maxNum));
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const day = document.getElementById('days'); 
+const todayDay = new Date();
+
+const days = () => {
+
+    week.forEach((item, i) => {
+        let createDiv = document.createElement('div');
+
+        if (i === +todayDay.getDay()-1) { 
+            console.log(todayDay.getDay());
+            createDiv.classList.add('today'); 
+            createDiv.textContent = week[i]; 
+        }
+        if (item === 'Суббота' || item === 'Воскресенье') {
+            createDiv.classList.add('italic'); 
+            createDiv.textContent = week[i]; 
+        } else {
+            createDiv.textContent = week[i]; 
+        }
+        day.appendChild(createDiv); 
+    });
+
 };
 
-// Число ли это?
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
- 
-const start = function() {
-    let rNum = randomNum(100);
-    // Начало игры 
-    let attempt = 10;
-    const game = function() {
-        attempt--;
-        if (attempt < 0) {
-            if (confirm('Попытки закончились, хотите сыграть еще?')) {
-                start();
-            } else {
-                alert('Игра окончена');
-                return;
-            }
-            } else {
-                const strNum = prompt('Угадай число от 1 до 100');
-                if (strNum === null) {
-                    alert('Игра окончена');
-                    return;
-                }
-                if (isNumber(strNum)) {
-                const num = +strNum;
-                if (num > rNum) {
-                    alert('Загаданное число меньше');
-                    game();
-                } else if (num < rNum) {
-                    alert('Загаданное число больше');
-                    game();
-                } else {
-                if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-                    start();
-                } else {
-                    alert('Конец игры! Возвращайтесь еще.');
-                    return;
-                }
-                }
-                } else {
-                    alert('Введи число!');
-                    game();
-                }
-            }
-    };
-    game();
-};
-
-// Запуск игры
-start();
+days(); 
