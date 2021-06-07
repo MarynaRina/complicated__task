f908e26254cd11cfb77a651ac6aa6a765a2f50c8
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-07 21:12:26 +0300

    Сделала усложненное задание 9

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const day = document.getElementById('days'); 
-const todayDay = new Date();
-
-const days = () => {
-
-    week.forEach((item, i) => {
-        let createDiv = document.createElement('div');
-
-        if (i === +todayDay.getDay()-1) { 
-            console.log(todayDay.getDay());
-            createDiv.classList.add('today'); 
-            createDiv.textContent = week[i]; 
+const showTime = () => {
+    const week = ['Воскресенье, ', 'Понедельник, ', 'Вторник, ', 'Среда, ','Четверг, ', 'Пятница, ', 'Суббота, '],
+          month = [' января ', ' февраля ', ' марта ',
+        ' апреля ', ' мая ', ' июня ',
+        ' июля ', ' августа ', ' сентября ',
+        ' октября ', ' ноября ', ' декабря '],
+         date = new Date();
+    const addZero = elem => {
+        if (String(elem).length === 1) { 
+            return '0' + elem; 
+        } else { 
+            return String(elem); 
         }
-        if (item === 'Суббота' || item === 'Воскресенье') {
-            createDiv.classList.add('italic'); 
-            createDiv.textContent = week[i]; 
-        } else {
-            createDiv.textContent = week[i]; 
-        }
-        day.appendChild(createDiv); 
-    });
+    };
+    const changeEnding = (num, timeElem = '') => {
+        const timeVariant = (timeElem === 'hr' ? [' час ', ' часа ', ' часов '] :
+            timeElem === 'min' ? [' минута ', ' минуты ', ' минут '] :
+            [' секунда ', ' секунды ', ' секунд ']);
+        const n = num % 10;
+        return num > 4 && num < 20 ? num + timeVariant[2] : 
+            n === 1 ? num + timeVariant[0] : 
+            n > 1 && n < 5 ? num + timeVariant[1] :
+            num + timeVariant[2];
+    };
 
+    const textTime = 'Сегодня ' + week[date.getDay()] + date.getDay() + month[date.getMonth()] + 
+    date.getFullYear() + ' года, ' + changeEnding(date.getHours(), 'hr') + 
+    changeEnding(date.getMinutes(), 'min') + changeEnding(date.getSeconds());
+    const time = addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' + 
+    addZero(date.getDay()) + '.' + addZero(date.getMonth()) + '.' + date.getFullYear();
+
+    document.querySelector('.text-time').textContent = textTime;
+    document.querySelector('.time').textContent = time;
+    
+    console.clear();
+    console.log(textTime);
+    console.log(time);
+    
 };
 
-days(); 
+let elem = document.createElement('div');
+elem.classList.add('text-time');
+document.body.appendChild(elem);
+
+elem = document.createElement('div');
+elem.classList.add('time');
+document.body.appendChild(elem);
+console.dir(elem);
+
+setInterval(showTime, 1000);
+showTime();
+document.body.style.margin = '400px 600px';
+document.body.style.fontSize = '20px';
